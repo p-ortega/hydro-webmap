02d78a072a42fded0db4219a10d7168a570911a2
Pablo Ortega <p-ortega@users.noreply.github.com>
2018-06-27 00:30:55 -0400

    second commit
    version 0.2
    plots changed to ggplot2 instead of plotly
    map clicking selection and plotting
    map doubleclicking,  point removed from plot
    map inbound selection and plotting
    changed popups to labes with htmltools

--- server.R
+++ server.R
@@ -1,83 +1,100 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-#
-
 library(shiny)
-
-## Layout plotly
-m = list(
-  l = 50,
-  r = 10,
-  b = 150,
-  t = 30,
-  pad = 0
-) 
-
-
-# EPSG32719 = leafletCRS(crsClass = "L.Proj.CRS", code = "EPSG:32719",
-#            proj4def = "+proj=utm +zone=19 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs", 
-#            resolutions = (20:1))
-
 
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
+  
   output$map <- renderLeaflet({ # Agrega mapa de leaflet
     leaflet()%>%
-    # Base groups
-    addProviderTiles(providers$OpenStreetMap, group = "Base")%>%
-    addProviderTiles(providers$Esri.WorldStreetMap, group = "Street")%>%
-    addProviderTiles(providers$Esri.WorldImagery, group = "Terrain")%>%  
-    
-    setView(-69.35, -22.85, zoom = 7)%>%
+      # Base groups
+      addProviderTiles(providers$OpenStreetMap, group = "Base")%>%
+      addProviderTiles(providers$Esri.WorldStreetMap, group = "Street")%>%
+      addProviderTiles(providers$Esri.WorldImagery, group = "Terrain")%>%  
       
-    # Add points
-    addCircleMarkers(data = pts, ~long, ~lat, popup = ~id, layerId = ~id,
-                     radius=5, group = "Monit. Points")%>%
-    
-    # Layers control
-    addLayersControl(
-      baseGroups = c("Base", "Street", "Terrain"),
-      options = layersControlOptions(collapsed = TRUE)
+      setView(-69.7, -23, zoom = 10)%>%
+      
+      # Add points
+      addCircleMarkers(data = pts, ~long, ~lat, layerId = ~id, 
+                       label = ~htmlEscape(id),
+                       radius=5, group = "Monitoring points")%>%
+      
+      # Layers control
+      addLayersControl(
+        baseGroups = c("Base", "Street", "Terrain"),
+        overlayGroups = c("Monitoring points"),
+        options = layersControlOptions(collapsed = TRUE)
       )%>%
-    
-    addMiniMap(position = 'bottomright', toggleDisplay = TRUE, collapsedWidth = 30, collapsedHeight = 30)
-      
+      addMiniMap(position = 'bottomright', toggleDisplay = TRUE, collapsedWidth = 30, collapsedHeight = 30)
+  })
+  
+  click = reactiveValues(clickedMarker=list())
+  
+  # store the click
+  observeEvent(input$map_marker_click,{
+    click2 = input$map_marker_click
+    click$clickedMarker = c(click$clickedMarker, click2$id)
+    dups = unique(click$clickedMarker[duplicated(click$clickedMarker)])
+    if(length(dups) > 0){
+      print("1")
+      print(dups)
+      click$clickedMarker = click$clickedMarker[-which(click$clickedMarker %in% dups)]
+      # g[-which(rownames(g) %in% remove), ]
+    }
+    else {
+      print("0")
+      click$clickedMarker = click$clickedMarker
+      }
+  })
+  
+ filtered.data = reactive({
+   na.omit(subset(gw.level, gw.level$id %in% click$clickedMarker))
+ })
+  
+ # A reactive expression that returns the set of points that are in bounds right now
+ ptsinbounds <- reactive({
+   if (is.null(input$map_bounds))
+     return(pts[FALSE,])
+   bounds <- input$map_bounds
+   latRng <- range(bounds$north, bounds$south)
+   lngRng <- range(bounds$east, bounds$west)
+   
+   a = subset(pts, lat >= latRng[1] & lat <= latRng[2] & long >= lngRng[1] & long <= lngRng[2])
+   na.omit(subset(gw.level, gw.level$id %in% a$id)) 
+   
+ })
+ 
+    # Plot subset of data selected by clicking
+  output$plot.clicked = renderPlot({
+    ggplot(filtered.data(), aes(x = date, y = level.mbgl, color = id)) + geom_line(size = 1.5) +
+      scale_y_reverse() +
+      theme_bw() + theme(panel.grid = element_blank())
     })
-
+  
+  # Plot subset of data in zoom bounds
+  output$plot.bounds = renderPlot({
+    if (nrow(ptsinbounds()) == 0)
+      return(NULL)
+    ggplot(ptsinbounds(), aes(x = date, y = level.mbgl, color = id)) + geom_line(size = 1.5) +
+      scale_y_reverse() +
+      theme_bw() + theme(panel.grid = element_blank())
+  })
+  
+  # filtered.pts = reactive({
+  #   subset(pts, pts$id %in% click$clickedMarker)
+  # })
+  
+  # change color of icon
+  # observe({
+  #   leafletProxy("map")%>%
+  #     clearMarkers(data = filtered.pts())%>%
+  #     addCircleMarkers(data = filtered.pts(),
+  #                      radius = 5, color = "red")
+  # })
+  
+  
   # Render table with data
   output$data <- renderDataTable({
     gw.level
   })
-  my.mapdata <- reactiveValues(clickedMarker=NULL)
-  # Oberserve when marker clicked
-  observeEvent(input$map_marker_click,{
-    print("observed map_marker_click")
-    my.mapdata$clickedMarker = input$map_marker_click
-    print(my.mapdata$clickedMarker$id)
-    my.leveldata = subset(gw.level, gw.level$id == my.mapdata$clickedmarker$id)
-    print(my.leveldata$id)
-    })
-  
-  # Render plot with groundwater levels
-  output$plot.level = renderPlotly({
-    plot_ly(gw.level, x = ~date, y = ~level.mbgl, 
-            color = ~id, type = "scatter", mode =  "lines",
-            marker = list(size = 8, line = list(color = ~id,
-                                                width = 1.5))) %>%
-      layout( width = 450, height = 400, margin = m,
-              yaxis = list(autorange = "reversed", zeroline = FALSE, showgrid = FALSE,
-                           showline = TRUE, title = "Groundwater Level [mbgl]", 
-                           ticks = "outside", nticks = 10, tickwidth = 2,
-                           linewidth = 2),
-              xaxis = list(zeroline = TRUE, showline = TRUE, title = FALSE, 
-                           showgrid = FALSE, ticks = "outside", nticks = 10,
-                           tickwidth = 2, linewidth = 2, tickangle = -90),
-              showlegend = FALSE)
-    
-  })  
   
 })
--- ui.R
+++ ui.R
@@ -5,42 +5,44 @@ shinyUI(fluidPage(
   
   # Application title
   navbarPage("Groundwater Data", id = 'nav', collapsible = TRUE, position = "fixed-top",
-  
-  # Sidebar with a slider input for number of bins 
-  tabPanel("Interactive map",
-  
-    div(class="outer",
-               
-        tags$head(
-        # Include  custom CSS
-        includeCSS("styles.css")
-        ),
-           
-    absolutePanel(id = "controls", class = "panel panel-default", 
-        fixed = TRUE, draggable = TRUE, style="z-index:500;",
-        top = 60, right = "auto", left = 60, bottom = "auto", width = 500, 
-        h2("Data explorer"), height = 650,
-        
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30),
-       
-       plotlyOutput("plot.level", height = "300px")
-    ),
-    
-    # Mapa de Leaflet 
-    
-      leafletOutput("map", height = "100%", width = "100%")
-    )
-  ),
-  
-  tabPanel("Data",
-           
-           dataTableOutput("data") 
-           
-           )
-  
+             
+             # Sidebar with a slider input for number of bins 
+             tabPanel("Interactive map",
+                      
+                      div(class="outer",
+                          
+                          tags$head(
+                            # Include  custom CSS
+                            includeCSS("styles.css")
+                          ),
+                          
+                          absolutePanel(id = "controls", class = "panel panel-default", 
+                                        fixed = TRUE, draggable = TRUE, style="z-index:500;",
+                                        top = 60, right = "auto", left = 60, bottom = "auto", width = 500, 
+                                        h2("Data explorer"), height = 650,
+                                        
+                                        sliderInput("bins",
+                                                    "Number of bins:",
+                                                    min = 1,
+                                                    max = 50,
+                                                    value = 30),
+                                        
+                                        plotOutput("plot.bounds", height = "200px"),
+                                        plotOutput("plot.clicked", height = "200px")
+                                        
+                          ),
+                          
+                          # Mapa de Leaflet 
+                          
+                          leafletOutput("map", height = "100%", width = "100%")
+                      )
+             ),
+             
+             tabPanel("Data",
+                      
+                      dataTableOutput("data") 
+                      
+             )
+             
   )
 ))
